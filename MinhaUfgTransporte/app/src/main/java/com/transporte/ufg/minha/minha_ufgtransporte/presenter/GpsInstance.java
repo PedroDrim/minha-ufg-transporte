0fffb2713011c6dc4f08ac3de50b5c378e9fd25b
moiseshilario <moiseshilariu@gmail.com>
2017-11-27 01:56:33 -0200

    Corrige gps permissions e centraliza local já salvo

--- MinhaUfgTransporte/app/src/main/java/com/transporte/ufg/minha/minha_ufgtransporte/presenter/GpsInstance.java
+++ MinhaUfgTransporte/app/src/main/java/com/transporte/ufg/minha/minha_ufgtransporte/presenter/GpsInstance.java
@@ -6,22 +6,32 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.transporte.ufg.minha.minha_ufgtransporte.view.MapActivity;
 
 import java.util.List;
-
-import static android.content.Context.LOCATION_SERVICE;
+import java.util.Map;
 
 /**
  * Created by pedro on 25/11/17.
  */
 
-public class GpsInstance {
+public class GpsInstance extends AppCompatActivity{
 
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private Activity activity;
     private boolean enable;
+    Location lastKnownLocation;
 
     public GpsInstance(Activity activity){
         this.enable = false;
@@ -32,41 +42,45 @@ public class GpsInstance {
 
         int gps = ContextCompat.checkSelfPermission(
                 this.activity,
-                Manifest.permission.READ_CONTACTS
+                Manifest.permission.ACCESS_FINE_LOCATION
         );
 
         if (gps != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(
-                    activity,
+            ActivityCompat.requestPermissions(activity,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
-            );
-
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
             this.enable = true;
         }
     }
 
-    public Location getLastKnownLocation() {
-        LocationManager mLocationManager =
-                (LocationManager) this.activity.getApplicationContext().getSystemService(LOCATION_SERVICE);
-
-        List<String> providers = mLocationManager.getProviders(true);
-        Location bestLocation = null;
-
-        for (String provider : providers) {
-
-            @SuppressLint("MissingPermission")
-            Location location = mLocationManager.getLastKnownLocation(provider);
-
-            if (location == null) continue;
-            if (bestLocation == null ||
-                    (location.getAccuracy() < bestLocation.getAccuracy())) bestLocation = location;
-        }
-        return bestLocation;
-    }
-
     public boolean isEnable(){
         return this.enable;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    this.enable = true;
+
+                    MapActivity map = (MapActivity) activity;
+                    map.getDeviceLocation();
+
+
+                } else {
+
+                    this.enable = false;
+                }
+                return;
+            }
+        }
     }
 }
--- MinhaUfgTransporte/app/src/main/java/com/transporte/ufg/minha/minha_ufgtransporte/view/CrudMapActivity.java
+++ MinhaUfgTransporte/app/src/main/java/com/transporte/ufg/minha/minha_ufgtransporte/view/CrudMapActivity.java
@@ -1,7 +1,9 @@
 package com.transporte.ufg.minha.minha_ufgtransporte.view;
 
 import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -9,12 +11,15 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.transporte.ufg.minha.minha_ufgtransporte.R;
 import com.transporte.ufg.minha.minha_ufgtransporte.model.Flag;
 import com.transporte.ufg.minha.minha_ufgtransporte.model.LocationTypesConverter;
@@ -32,6 +37,7 @@ public class CrudMapActivity extends AppCompatActivity implements OnMapReadyCall
     private GoogleMap mMap;
     private GpsInstance gpsInstance;
     private CrudMapClickListener crudMapClickListener;
+    private Location mLastKnownLocation;
 
     public CrudMapActivity(){
         this.crudMapClickListener = new CrudMapClickListener();
@@ -86,28 +92,26 @@ public class CrudMapActivity extends AppCompatActivity implements OnMapReadyCall
         this.mMap = googleMap;
         this.mMap.setMinZoomPreference(10.0f);
 
-        LatLng goiania;
-        LatLng lastKnownLocation = LocationTypesConverter.locationToAndroidLatLng(
-                this.gpsInstance.getLastKnownLocation()
-        );
-
 
         if(this.gpsInstance.isEnable()) {
             this.mMap.setMyLocationEnabled(true);
             this.mMap.getUiSettings().setMyLocationButtonEnabled(true);
-
-            goiania = lastKnownLocation;
-        } else {
-            goiania = new LatLng(-16.665136, -49.286041);
         }
 
-        this.mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(goiania, 15));
+        MyPlace myPlace = EventBus.getDefault().getStickyEvent(MyPlace.class);
+
+        if(myPlace != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myPlace.getLatitude(), myPlace.getLongitude()), 15));
+        }
+        else {
+            LatLng goiania = new LatLng(-16.665136, -49.286041);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(goiania, 15));
+        }
 
         String flagKey = this.getString(R.string.flag_key);
         int flag = this.getIntent().getIntExtra(flagKey, -1);
 
         if(flag == Flag.UPDATE.valor){
-            MyPlace myPlace = EventBus.getDefault().getStickyEvent(MyPlace.class);
             this.mMap.addMarker(
                     new MarkerOptions().position(myPlace.toLatLng())
             );
@@ -123,6 +127,39 @@ public class CrudMapActivity extends AppCompatActivity implements OnMapReadyCall
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_locais, menu);
         return true;
+    }
+
+    private void getDeviceLocation() {
+    /*
+     * Get the best and most recent location of the device, which may be null in rare
+     * cases when a location is not available.
+     */
+
+        FusedLocationProviderClient mFusedLocationProviderClient = new FusedLocationProviderClient(this);
+
+        try {
+            if (this.gpsInstance.isEnable()) {
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = (Location) task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), 15));
+                        } else {
+                            LatLng goiania = new LatLng(-16.665136, -49.286041);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(goiania, 15));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 
 }
